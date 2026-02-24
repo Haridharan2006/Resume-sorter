c06bc21ec2a43083f9a9f4d21532c73b03edcde2
Haridharan B S <haridharanbs2006@gmail.com>
2026-02-24 21:58:58 +0530

    Add files via upload

---
+++ utils/matcher.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def get_match_score(resume_text, job_desc):
+    resume_embedding = model.encode(resume_text, convert_to_tensor=True)
+    job_embedding = model.encode(job_desc, convert_to_tensor=True)
+    similarity = util.cos_sim(resume_embedding, job_embedding)
+    return float(similarity[0][0]) * 100
---
+++ utils/social_fetch.py
@@ -0,0 +1,21 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="YOUR_CLIENT_ID",
+    client_secret="YOUR_CLIENT_SECRET",
+    user_agent="resume_screening_app"
+)
+
+def fetch_reddit_posts(profile_url, limit=20):
+    username = profile_url.split("/")[-2]
+    user = reddit.redditor(username)
+
+    posts = []
+
+    for submission in user.submissions.new(limit=limit):
+        posts.append(submission.title + " " + submission.selftext)
+
+    for comment in user.comments.new(limit=limit):
+        posts.append(comment.body)
+
+    return posts
